f2aa45c81b843c8b9cc9c4221df7492958a5c473
Taylor1990 <has728975@yandex.ru>
2017-04-04 23:56:53 +0300

    Finish

--- scripts/main.js
+++ scripts/main.js
@@ -55,24 +55,6 @@
 // };
 
 $(function() {
-    var Router = Backbone.Router.extend({
-        routes: {
-            "": 'start',
-            '!/': 'start',
-            "!/add": 'addProduct'
-        },
-        start: function(){
-
-        },
-        addProduct: function(){
-
-        }
-    });
-
-    var router = new Router();
-
-    Backbone.history.start();
-
     var Product = Backbone.Model.extend({
         defaults: {
           title: '',
@@ -93,13 +75,95 @@ $(function() {
     var MainView = Backbone.View.extend({
         el: $('.wrapper'),
         initialize: function(){
-            this.$el.append('something');
 
         },
+        events: {
+          "click button": "onClickAdd",
+          "click li": "editProduct"
+        },
         render: function(){
-            this.$el.append('something');
+          this.$el.html('');
+
+          this.$el.append('<ul>' + Products.map(function(item){
+            return '<li id = ' + item.cid + '><div>' + item.get('title') + '</div><div>' + item.get('price') + '</div></li>'
+          }).join('') + '</ul>');
+
+          this.$el.append('<button>Добавить</button>');
+
+        },
+        onClickAdd(e){
+          router.navigate("/add", {trigger: true});
+        },
+        editProduct(e){
+          router.navigate('/edit/' + e.currentTarget.id, {trigger: true});
         }
     });
 
-    var App = new AppView();
+    var AddView= Backbone.View.extend({
+      el: $('.wrapper'),
+      events: {
+        "click button": "addFunction"
+      },
+      addFunction: function(e){
+        var title = $('#title').val();
+        var price = $('#price').val();
+
+        Products.add(new Product({title: title, price: price}))
+
+        router.navigate('/', {trigger: true});
+      },
+      render: function(){
+        this.$el.html('');
+        this.$el.append('<div><div>Название</div><input id = "title" type = "text"></div>');
+        this.$el.append('<div><div>Цена</div><input id = "price" type = "text"></div>');
+        this.$el.append('<button>Добавить</div>');
+      }
+    });
+
+    var EditView = Backbone.View.extend({
+      el: $('.wrapper'),
+      events: {
+        "click button": "onClickEdit"
+      },
+      cid: '',
+      onClickEdit: function(){
+        var title = this.$el.find('#title').val();
+        var price = this.$el.find('#price').val();
+        Products.get(this.cid).set({title: title, price: price});
+        router.navigate('/', {trigger: true});
+      },
+      render: function(id){
+        this.$el.html('');
+        this.$el.append('<div><div>Название</div><input id = "title" type = "text"></div>');
+        this.$el.append('<div><div>Цена</div><input id = "price" type = "text"></div>');
+        this.$el.find('#title').val(Products.get(id).get('title'));
+        this.$el.find('#price').val(Products.get(id).get('price'));
+        this.$el.append('<button>Изменить</div>');
+        this.cid = id;
+      }
+    })
+
+    var Router = Backbone.Router.extend({
+        routes: {
+            "": 'start',
+            "add": 'addProduct',
+            "edit/:id": "editProduct"
+        },
+        start: function(){
+          var mainView = new MainView;
+          mainView.render();
+        },
+        addProduct: function(){
+          var addView = new AddView;
+          addView.render();
+        },
+        editProduct: function(id){
+          var editView = new EditView;
+          editView.render(id)
+        }
+    });
+
+    var router = new Router();
+
+    Backbone.history.start();
 });
