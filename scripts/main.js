502fc000a2fcf55e8e2535a04c12be7f247f478a
Taylor1990 <has728975@mail.ru>
2017-04-04 15:03:29 +0300

    s

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,105 @@
-window.onload = function(){
-  var app = {
-    models: {}
-  };
+// window.onload = function(){
+//   var app = {
+//     models: {}
+//   };
+//
+//   app.models.Product = Backbone.Model.extend({
+//     defaults: {
+//       id: 0,
+//       title: '',
+//       price: ''
+//     }
+//   });
+//
+//   var product1 = new Product({id: 1, title: 'book',price: '400p'}, {collection: Products});
+//   var product2 = new Product({id: 2, title: 'oil',price: '150p'}, {collection: Products});
+//   var product3 = new Product({id: 3, title: 'meat',price: '200p'}, {collection: Products});
+//
+//   app.models.Products = new Backbone.Collection([product1, product2, product3]);
+//
+//   app.models.ProductView = new Backbone.View.extand({
+//     tagname: 'div',
+//     className: 'product',
+//
+//     render: function(){
+//       var data = this.model.toJSON();
+//       var self = this;
+//       $(this.el).html('');
+//       $(this.el).append($('<div></div>').text(data.title));
+//       $(this.el).append($('<div></div>').text(data.price));
+//       return this;
+//     }
+//   });
+//
+//   app.models.Page = new Backbone.View.extend({
+//     el: null,
+//     Products: null,
+//
+//     initialize: function(){
+//       this.bind('page:load', this.pageLoad, this)
+//       this.Products = app.models.Products;
+//       return this;
+//     },
+//
+//     pageLoad: function(data){
+//       var self = this;
+//       this.el = ($('.wrapper'));
+//     },
+//
+//     render: function(ret){
+//
+//     }
+//   });
+//
+//   app.models.Page.trigger('page:load');
+// };
 
-  var Product = Backbone.Model.extend({
-    defaults: {
-      id: 0,
-      title: '',
-      price: ''
-    }
-  })
+$(function() {
+    var Router = Backbone.Router.extend({
+        routes: {
+            "": 'start',
+            '!/': 'start',
+            "!/add": 'addProduct'
+        },
+        start: function(){
 
-  var product1 = new Product({id: 1, title: 'book',price: '400p'}, {collection: Products});
-  var product2 = new Product({id: 2, title: 'oil',price: '150p'}, {collection: Products});
-  var product3 = new Product({id: 3, title: 'meat',price: '200p'}, {collection: Products});
+        },
+        addProduct: function(){
 
-  var Products = new Backbone.Collection([product1, product2, product3]);
+        }
+    });
 
-  var ProductView = new Backbone.View.extand({
-    tagname: 'div',
-    className: 'product',
+    var router = new Router();
 
-    render: function(){
-      var data = this.model.toJSON();
+    Backbone.history.start();
 
-      $(this.el).html('').data('rowId', data.id);
-      
-    }
-  })
-}
+    var Product = Backbone.Model.extend({
+        defaults: {
+          title: '',
+          price: ''
+        }
+    });
+
+    var ProductsList = Backbone.Collection.extend({
+        model: Product
+    });
+
+    var Products = new ProductsList([
+        new Product({title: 'book',price: '400p'}),
+        new Product({title: 'oil',price: '150p'}),
+        new Product({title: 'meat',price: '200p'})
+    ]);
+
+    var MainView = Backbone.View.extend({
+        el: $('.wrapper'),
+        initialize: function(){
+            this.$el.append('something');
+
+        },
+        render: function(){
+            this.$el.append('something');
+        }
+    });
+
+    var App = new AppView();
+});
